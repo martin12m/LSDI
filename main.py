13bc68523a1805de4237b7c8910ea49538abbfe4
martin12m <105725408+martin12m@users.noreply.github.com>
2024-12-02 02:59:59 +0100

    added main and database processing

---
+++ main.py
@@ -0,0 +1,8 @@
+from database_processing import process_all_databases
+
+def main():
+    # Step 1: Process all databases and save the tables as .csv
+    process_all_databases()
+
+if __name__ == "__main__":
+    main()
