4e7c4503ff56fca7039263b16f76fda26a45aa44
anshi43 <mavaniankit09@gmail.com>
2025-02-14 18:52:03 +0100

    removed unnecessary files

--- Files before Milestone/TrainingModel_output.py
+++
@@ -1,3 +0,0 @@
-from tensorflow import keras
-model = keras.models.load_model('cnn_model.h5')
-model.summary()
--- Files before Milestone/main.py
+++
@@ -1,8 +0,0 @@
-from database_processing import process_all_databases
-
-def main():
-    # Step 1: Process all databases and save the tables as .csv
-    process_all_databases()
-
-if __name__ == "__main__":
-    main()
